c77d1cb72c246ad5d8eb46c7fc0081480eef2f57
MarysolMurilloC <marysolm1999@gmail.com>
2021-05-23 00:40:03 -0600

    23-5

--- page-tienda.php
+++ page-tienda.php
@@ -1,22 +1,20 @@
 <?php get_header(); ?>
 
+    <div class="bg-tien">
     <?php
         //Loop
         while( have_posts() ) : the_post();
     ?>
 
-    <div class="row">
-        <div class="col-11 offset-1">
-            <h3><?php the_title(); ?></h3>
-        </div>
-    </div>
-
     <div class="row justify-content-center">
         <div class="row-10">
+			<h2<?php the_title(); ?></h2>
         <p><?php the_content(); ?></p>
         </div>
     </div>
 
     <?php endwhile; ?>
+    </div>
+    
 
 <?php get_footer(); ?>
