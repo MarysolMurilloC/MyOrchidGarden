ec5b15679b1b41401ff9161bd86f891623c01c9b
MarysolMurilloC <marysolm1999@gmail.com>
2021-07-21 09:58:23 -0600

    404

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+    <div class="bg-der">
+        <div class="bg-contform">
+
+        <h2>ERROR 404</h2>
+		<P>Esta página no está disponible</P>
+    </div>
+    
+  
+<?php get_footer(); ?>
